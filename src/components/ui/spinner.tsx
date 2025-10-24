4d81681e60b16ff12c3a525b1fe218d62f435dff
Tanay Lakhani <tpl253@nyu.edu>
2025-10-24 12:52:29 +0530

    fixing type error

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,16 +1,26 @@
 import { LoaderIcon } from 'lucide-react'
+import { forwardRef } from 'react'
 
 import { cn } from '@/lib/utils'
 
-function Spinner({ className, ...props }: React.ComponentProps<'svg'>) {
-  return (
-    <LoaderIcon
-      role="status"
-      aria-label="Loading"
-      className={cn('size-4 animate-spin', className)}
-      {...props}
-    />
-  )
+interface SpinnerProps extends Omit<React.ComponentProps<'svg'>, 'ref'> {
+  className?: string
 }
+
+const Spinner = forwardRef<SVGSVGElement, SpinnerProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <LoaderIcon
+        ref={ref}
+        role="status"
+        aria-label="Loading"
+        className={cn('size-4 animate-spin', className)}
+        {...props}
+      />
+    )
+  }
+)
+
+Spinner.displayName = 'Spinner'
 
 export { Spinner }
